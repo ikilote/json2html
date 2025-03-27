123b6a0ba9d0d50460fae6f4e89592f71f14ecfc
Zéfling <zefling@ikilote.net>
2025-03-27 23:58:48 +0100

    Fix package & tsconfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MagmaInput, MagmaInputTextarea } from '@ikilote/magma';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JsPipe } from './js.pipe';
 
 @NgModule({
     declarations: [AppComponent, JsPipe],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, MagmaInput, MagmaInputTextarea],
     providers: [],
     bootstrap: [AppComponent],
 })
